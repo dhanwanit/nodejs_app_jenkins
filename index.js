5a6cf95ca5fc49cbed55c2386cf3cab8a9a9c742
dhr <dhr@dhr.com>
2022-03-21 06:59:28 +0000

    github webhooks with

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App Vfdasf!')
+  res.send('CICD App Vfdasasdfaf!')
 })
 
 app.get('/status', (req, res) => {
